22d05099acef035cdccf99065574a4baca116b0e
wisdomyeh <0632026@gmail.com.tw>
2021-03-16 17:22:29 +0800

    add css settings

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <!DOCTYPE html>
 <html>
-<title>葉智慧的網頁</title>
-<body>
+ <head>
+ <title>葉智慧的網頁</title>
+<style>
+	body {
+  background-color: lightblue;
+}
 
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+</style>
+</head>
+<body>
 <h1>智慧的星球</h1>
 <p>This is a paragraph.</p>
 
